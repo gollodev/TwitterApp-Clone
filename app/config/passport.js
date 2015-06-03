c9c825ccdba6bb447f79dd8bcf62335bb4face8c
Jose Lopez <gollo_autumn@live.com>
2015-06-02 21:48:30 -0430

    fixing bugs passport.js

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,7 +1,7 @@
 
-var TwitterStrategy  = require('passport-twitter').Strategy;
-var configTwitter    = require('./configTwitter');
-var UserModel		 = require('../models/UserModel');
+var TwitterStrategy  = require('passport-twitter').Strategy,
+    configTwitter    = require('./configTwitter'),
+    UserModel		 = require('../models/UserModel');
 
 module.exports = function (passport) {
 
@@ -29,25 +29,23 @@ module.exports = function (passport) {
     // User.findOne won't fire until we have all our data back from Twitter
         process.nextTick(function() {
 
-            UserModel.findOne({ 'twitter.id' : profile.id }, function(err, user) {
+            UserModel.findOne({ _id : profile.id }, function(err, user) {
 
                 // if there is an error, stop everything and return that
                 // ie an error connecting to the database
-                if (err)
-                    return done(err);
+                if (err) return done(err);
 
                 // if the user is found then log them in
                 if (user) {
                     return done(null, user); // user found, return that user
                 } else {
                     // if there is no user, create them
-                    var newUser                 = new UserModel();
-
-                    // set all of the user data that we need
-                    newUser.twitter.id          = profile.id;
-                    newUser.twitter.token       = token;
-                    newUser.twitter.username    = profile.username;
-                    newUser.twitter.displayName = profile.displayName;
+                    var newUser = new UserModel({
+                        _id         : profile.id,
+                        token       : token,
+                        username    : profile.username,
+                        displayName : profile.displayName
+                    });                   
 
                     // save our user into the database
                     newUser.save(function(err) {
